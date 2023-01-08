034bca010087bcbdb273539040e76ca39963ef1c
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-01-08 11:22:42 -0500

    Add files via upload

---
+++ tictactoegame.py
@@ -0,0 +1,6 @@
+##Here, I am learning how to create a simple tic-tac-toe game
+##Code learned from CDcodes
+
+#1) creating the tic tac toe board
+gameBoard = ("|a||b||c|\n|d||e||f|\n|g||h||i|")
+print(gameBoard)
