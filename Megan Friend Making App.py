a8674fabc62c3a273ebf0d8b563b244f0fce17ee
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-02-01 21:48:44 -0500

    Update Megan Friend Making App.py

--- Megan Friend Making App.py
+++ Megan Friend Making App.py
@@ -4,7 +4,7 @@
 from tkinter import *
 from tkinter import ttk
 
-#------First - make a user interface window--------
+#------make a user interface window--------
 #making a class
 class theToDoList:
     def __init__(self, root):
@@ -44,6 +44,20 @@ class theToDoList:
         self.textBox = Text(self.root,font= "Times, 20 bold", height = 2.25, width=30,bd=10)
         self.textBox.place(x=20, y = 170)
 
+    #-------Making the function so that user can add and delete a resource on list-------------
+        def addResource():
+            resourceInput = self.textBox.get(1.0,END)
+            #the get(1.0,END) means it will focus on everything typed in the box
+            self.boxResources.insert(END,resourceInput) #this gets the text in the text box and puts it into the lsit of resources box
+            with open("appData.txt","a") as file: #w means to write, a means append
+                file.write(resourceInput)
+                file.seek(0)
+                file.close()
+            self.textBox.delete(1.0,END) #this removes what we typed in the add resource box after user presses "add resource"
+
+        def deleteResource():
+            removeResource = self.boxResources.curselection()
+            getResourceToDelete = self.boxResources.get(removeResource)
 #starting execution of program point
 def main():
     root = Tk() #tk helps bring many tools for the application I am making
@@ -58,4 +72,4 @@ if __name__ == "__main__":
     main()
 
 #Video used: https://www.youtube.com/watch?v=mmpVsw8aXi4&list=PLXCw5VdOQb7gCT0gC5jg66carWh48JcOy&index=4 
-# bookmark https://youtu.be/mmpVsw8aXi4?t=995
+# bookmark https://youtu.be/mmpVsw8aXi4?t=1409
