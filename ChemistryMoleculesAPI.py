7b72aeb6590ef36a53c95db3b88566087590b040
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-02-06 19:36:51 -0500

    Add files via upload

---
+++ ChemistryMoleculesAPI.py
@@ -0,0 +1,12 @@
+# Practicing using API- Python
+#reference website: https://www.dataquest.io/blog/python-api-tutorial/
+# I will practice using API with from mnowotka's JSON of chemical molecules
+
+#getting some data from a given webstie so we can see its status (404, 200, etc.)
+import requests
+response = requests.get("https://www.ebi.ac.uk/chembl/api/data/molecule/CHEMBL25.json")
+
+print(response.status_code)
+
+#seeing the data from the JSON we are using
+print(response.json())
