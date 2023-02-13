597a2a1b43c899b1ae131efedfd787ee27a2dfc1
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-02-13 16:52:35 -0500

    Update WeatherIrelandAPI.py

--- WeatherIrelandAPI.py
+++ WeatherIrelandAPI.py
@@ -28,4 +28,23 @@ output_json = response.json()
 wind = output_json["wind"]["speed"]
 
 #utilizing variables in print statements
-print("The wind speed outside is " + str(wind) + " mph")
+print("The wind speed in Ireland is " + str(wind) + " mph")
+
+#Creating class to have certain information about something specific
+class City:
+    def __init__(self,name, feels_like, units="metric"):
+        self.name = name
+        self.feels_like= feels_like
+        self.units = units
+    #we need a new function in the class that calls for the API
+    def retrieveInfo(self):
+        try:
+            response = requests.get("https://api.openweathermap.org/data/2.5/weather?units={self.units}&lat=53.1424&lon=7.6921&appid=6cd640b6d94034814baef94446b8b3ff")
+
+        except:
+            print("An error occured.")
+
+        output_json = response.json()
+        self.wind = output_json["wind"]["speed"]
+        print("The wind speed in Ireland is " + str(wind) + " mph")
+        #self helps us access all instances of a specific class
