02159d8aaaa44c76eab6b862635bf3147bc3a49f
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-02-18 20:45:03 -0500

    Update hangmanChemistry.py

--- hangmanChemistry.py
+++ hangmanChemistry.py
@@ -4,14 +4,57 @@
 #Hangman game code based on: https://www.youtube.com/watch?v=cJJTnI22IF8
 
 #Here, I want to create a storage space for the images of hangman and chemistry terms
+nanoputian  = ['''
++-------------------------------+
+              |                 |
+     \|/     _|_     \|/        |
+      \     /   \    /          |
+      \\\   O   O  ///          |
+       \\\   \ /  ///           |
+         \    |   /             |
+          \ //  \/              |
+          | /    ||             |
+          |      ||             | 
+          \\      /             | 
+            \\  /               |
+              |                 |
+              |                 |
+            /   \               |
+           /     \              |
+     ____/        \____         |
+                                |
+                                |
+                                |
+                                |
+========================================
+
+
+''']
+
+
 chemHangmanIcons = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
++-------------------------------+
+              |                 |
+              |                 |
+                                |
+                                |
+                                |
+                                |
+                                |
+                                |
+                                | 
+                                | 
+                                |
+                                |
+                                |
+                                |
+                                |
+                                |
+                                |
+                                |
+                                |
+                                |
+========================================''', '''
   +---+
   |   |
   O   |
@@ -47,19 +90,49 @@ chemHangmanIcons = ['''
  /    |
       |
 =========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
++-------------------------------+
+              |                 |
+     \|/     _|_     \|/        |
+      \     /   \    /          |
+      \\\   O   O  ///          |
+       \\\   \ /  ///           |
+         \    |   /             |
+          \ //  \/              |
+          | /    ||             |
+          |      ||             | 
+          \\      /             | 
+            \\  /               |
+              |                 |
+              |                 |
+            /   \               |
+           /     \              |
+     ____/        \____         |
+                                |
+                                |
+                                |
+                                |
+========================================
 
-#Word bank of General Chemistry terms
-genChemWords = ("acid, Arrhenius, base, Bronsted, chemistry, dissociation, density, electron, element"
-          "Freezing, functional group, Gibbs free energy, Gamma emission, Half-reaction, Halogens, indicator, Ionic bond" 
-          "Kinetic energy, kinetic theory, Le Châtelier's principle, Lattice energy, molarity, molality, nitrogen, Neutron"
-          "osmotic pressure, Oxidation, peroxide, Periodic table, Radioactive decay, Raoult's law"
-          "Supersaturated, Salt bridge, Thermodynamics, temperature, Uncertainty principle, Unimolecular reaction"
-          "VSPER theory, valence electron, Wavelength").split()
+
+''']
+
+#Word bank of General Chemistry terms, this is made in a different python file
+#genChemWords = ("acid, Arrhenius, base, Bronsted, chemistry, dissociation, density, electron, element"
+         # "Freezing, functional group, Gibbs free energy, Gamma emission, Half-reaction, Halogens, indicator, Ionic bond" 
+          #"Kinetic energy, kinetic theory, Le Châtelier's principle, Lattice energy, molarity, molality, nitrogen, Neutron"
+          #"osmotic pressure, Oxidation, peroxide, Periodic table, Radioactive decay, Raoult's law"
+          #"Supersaturated, Salt bridge, Thermodynamics, temperature, Uncertainty principle, Unimolecular reaction"
+          #"VSPER theory, valence electron, Wavelength").split()
+
+#setting up game
+
+#1)getting random word for user to guess
+import random
+from GeneralChemistryTerms import genChemWords
+
+def useWord(genChemWords):
+  genChemWord = random.choice(genChemWords)
+
+
+
  
