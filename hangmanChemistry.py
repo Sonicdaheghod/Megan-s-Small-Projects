e4f48cb7f8bb9a5280b6007fce5528242ec9ff47
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-02-22 21:15:42 -0500

    Update hangmanChemistry.py

--- hangmanChemistry.py
+++ hangmanChemistry.py
@@ -2,7 +2,7 @@
 
 #credit for hangman and wordbank template: <script src="https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c"
 #Hangman game code based on: https://www.youtube.com/watch?v=cJJTnI22IF8
-#timestamp:https://youtu.be/cJJTnI22IF8?t=271
+#timestamp:https://youtu.be/cJJTnI22IF8?t=312
 
 #Here, I want to create a storage space for the images of hangman and chemistry terms
 nanoputian  = ['''
@@ -230,6 +230,11 @@ def chemHangman():
   if userAttempt in chemAlphabet - lettersGuessed:
     userAttempt.add(userAttempt)
 
+
+
+
+
+ 
 
 
 
